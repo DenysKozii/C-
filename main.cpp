91f71f92598913b86936146474a3329ef35ee1ff
DenysKozii <61294707+DenysKozii@users.noreply.github.com>
2021-01-27 09:01:34 +0200

    Task 1
    Denys Kozii

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int c;
+    double F;
+    cout<< "enter temperature c: ";
+    cin >> c;
+    F = c*9.0/5.0+32.0;
+    cout << F << endl;
+    return 0;
+}
