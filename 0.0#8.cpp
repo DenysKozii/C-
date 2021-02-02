aee815998d0da4e0bdff7c64702cc3e98b2e78a1
DenysKozii <61294707+DenysKozii@users.noreply.github.com>
2021-02-02 15:45:09 +0200

    Add files via upload
    Козій Денис

---
+++ 0.0#8.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    double a;
+    scanf("%lf", &a);
+    printf("%.3lf", log10(a));
+    return 0;
+}
