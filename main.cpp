c54380ea369a037106415805dd167d73a1a77258
DenysKozii <61294707+DenysKozii@users.noreply.github.com>
2021-02-03 09:05:04 +0200

    Козій Денис теперь через Біном

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
+double f(double x){
+    return pow(x+1,5);
+}
 int main()
 {
-    int c;
-    double F;
-    cout<< "enter temperature c: ";
-    cin >> c;
-    F = c*9.0/5.0+32.0;
-    cout << F << endl;
+    double x;
+    cin >> x;
+    cout << f(x) << endl;
     return 0;
 }
